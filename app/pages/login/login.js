bd182769c5c4b58de60a531f13c4172d4e02c4ab
Daniel Lamberti <daniellamberti@gmail.com>
2025-01-22 12:03:30 -0300

    Add logout logic to login.js

--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -23,7 +23,7 @@ async function logUser(event) {
 
   try {
 
-    const response = await fetch('https://097c-2800-2260-4040-1a92-588e-986a-7bb2-fa63.ngrok-free.app/auth/login', {
+    const response = await fetch('https://5910-2800-2260-4040-1a92-49d3-f5a0-e305-aba0.ngrok-free.app/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
