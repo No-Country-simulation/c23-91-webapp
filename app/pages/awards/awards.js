7c5bdf8cd20bb484663d99ad1fbcda312233b422
Daniel Lamberti <daniellamberti@gmail.com>
2025-01-24 17:33:38 -0300

    Add awards.html, awards.css, awards.js

---
+++ app/pages/awards/awards.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    function fetchDonationData() {
+      fetch('./mock-response.json')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          const donationsTableBody = document.getElementById('donationsTableBody');
+          donationsTableBody.innerHTML = '';
+           
+          data.forEach((donation, index) => {
+             
+            const row = document.createElement('tr');
+  
+             
+            if (index % 2 === 0) {
+              row.classList.add('table-light'); 
+            } else {
+              row.classList.add('table-primary'); 
+            }
+  
+             
+            const numberCell = document.createElement('td');
+            numberCell.textContent = donation.number;
+  
+            const dateCell = document.createElement('td');
+            dateCell.textContent = donation.date;
+  
+            const hospitalCell = document.createElement('td');
+            hospitalCell.textContent = donation.hospital;
+  
+            const addressCell = document.createElement('td');
+            addressCell.textContent = donation.address;
+  
+             
+            row.appendChild(numberCell);
+            row.appendChild(dateCell);
+            row.appendChild(hospitalCell);
+            row.appendChild(addressCell);
+  
+            donationsTableBody.appendChild(row);
+          });
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+          alert('An error occurred while fetching the data.');
+        });
+    }
+  
+     
+    fetchDonationData();
+  });
+  
