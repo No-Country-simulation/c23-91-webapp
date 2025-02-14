599268a51848aa264936946635fabff0d41e2a64
Daniel Lamberti <daniellamberti@gmail.com>
2025-02-14 05:08:20 -0300

    --no-edit

--- app/pages/schedule_appointment/sched_appointment.js
+++ app/pages/schedule_appointment/sched_appointment.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authorization: ` Bearer ${localStorage.getItem("token")}`,
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         }
       );
