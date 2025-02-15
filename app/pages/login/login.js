4c06211d56f67355902d24f65690dfb25ec11b9f
gaboparra <geparra06@gmail.com>
2025-02-14 23:17:11 -0300

    fetch change

--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -23,7 +23,7 @@ togglePasswordButton.addEventListener("click", () => {
   };
 
   try {
-    const response = await fetch("http://localhost:8080/auth/login", {
+    const response = await fetch("https://reddevida.onrender.com/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
