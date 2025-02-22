a2cfba1368957a7383468bab030e727800e755e9
gaboparra <geparra06@gmail.com>
2025-02-22 01:07:27 -0300

    good version with netlify origin in cors

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "http://localhost:8080",
-      "https://c23-91-webapp-nfvs.onrender.com"
+      "https://c23-91-webapp-nfvs.onrender.com",
+      "https://red-de-vida.netlify.app" // Dominio en Netlify
     ],
   })
 );
